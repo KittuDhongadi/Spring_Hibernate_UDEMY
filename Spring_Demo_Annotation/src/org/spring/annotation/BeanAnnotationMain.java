6ea56e1e80fce9602806e1ba57a876bad1e56b70
Spring Framework <kishan.dhongadi@gmail.com>
2018-04-23 23:16:33 +0530

    @Component Tag of creation bean

---
+++ Spring_Demo_Annotation/src/org/spring/annotation/BeanAnnotationMain.java
@@ -0,0 +1,19 @@
+package org.spring.annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanAnnotationMain {
+	
+	public static void main(String args[])
+	{
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		
+		//retrieve bean Spring Container
+		Coach theCoach = context.getBean("SillyCoach",Coach.class);
+		
+		System.out.println(theCoach.getDailyWorkout());
+		
+		context.close();
+		}
+}
---
+++ Spring_Demo_Annotation/src/org/spring/annotation/TennisCoach.java
@@ -0,0 +1,13 @@
+package org.spring.annotation;
+
+import org.springframework.stereotype.Component;
+
+@Component("SillyCoach")
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "practice your backhand colley";
+	}
+
+}
