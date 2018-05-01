f1c0f8977e39249b6321f295abdb7c5dd90c0a7a
Spring Framework <kishan.dhongadi@gmail.com>
2018-05-01 16:51:02 +0530

    Added an @InitBinder Annotation to remove the white spaces

--- Sect15_SpringMVCFormValidation139-151/src/org/spring/MVC_139_151/CustomerController.java
+++ Sect15_SpringMVCFormValidation139-151/src/org/spring/MVC_139_151/CustomerController.java
@@ -2,15 +2,30 @@ package org.spring.MVC_139_151;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	//add an initbinder to convert trim inputStrings
+	//remove leading and trailing whitespaces
+	//resolve issue for our validation
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder)
+	{
+		StringTrimmerEditor stringTrimmerEditor = new  StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+
 	@RequestMapping("/showForm")
 	private String showForm(Model themodel)
 	{
@@ -21,12 +36,15 @@ public class CustomerController {
 	@RequestMapping("/processForm")
 	private String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult)
 	{
+		System.out.println("last name : |"+theCustomer.getLastName()+"|");
 		if(theBindingResult.hasErrors())
 		{
 			return "customer-form";
 		}
 		else
 		{
+			System.out.println("last name : |"+theCustomer.getLastName()+"|");
+
 			return "customer-confirmation";
 		}
 	}
